22d2ec4b48dbbf383c2d8afb4bcdc1d4c858b9cf
michael-kleinzit <michaelkl@wix.com>
2026-02-18 00:28:33 +0200

    fix tree :q

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -33,8 +33,8 @@ return {
         local floating_wins = {}
         local wins = vim.api.nvim_list_wins()
         for _, w in ipairs(wins) do
-          local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
-          if bufname:match 'neo%-tree' then table.insert(tree_wins, w) end
+          local buf = vim.api.nvim_win_get_buf(w)
+          if vim.bo[buf].filetype == 'neo-tree' then table.insert(tree_wins, w) end
           if vim.api.nvim_win_get_config(w).relative ~= '' then table.insert(floating_wins, w) end
         end
         if #wins - #floating_wins - #tree_wins == 1 then
